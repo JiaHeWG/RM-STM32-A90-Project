95719f81d40a7ca1f50adf2155d5c46d0be76d84
JiaHeWG <bill.grantter@outlook.com>
2026-07-10 19:53:34 +0800

    First commit: Enable Blink on given initial board

---
+++ Core/Src/gpio.c
@@ -0,0 +1,79 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file    gpio.c
+  * @brief   This file provides code for the configuration
+  *          of all used GPIO pins.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "gpio.h"
+
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/*----------------------------------------------------------------------------*/
+/* Configure GPIO                                                             */
+/*----------------------------------------------------------------------------*/
+/* USER CODE BEGIN 1 */
+
+/* USER CODE END 1 */
+
+/** Configure pins as
+        * Analog
+        * Input
+        * Output
+        * EVENT_OUT
+        * EXTI
+*/
+void MX_GPIO_Init(void)
+{
+
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(Onboard_LED_GPIO_Port, Onboard_LED_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, DIS_A_Pin|DIS_B_Pin|DIS_C_Pin|DIS_D_Pin
+                          |DIS_E_Pin|DIS_F_Pin|DIS_G_Pin|DIS_DP_Pin, GPIO_PIN_SET);
+
+  /*Configure GPIO pin : Onboard_LED_Pin */
+  GPIO_InitStruct.Pin = Onboard_LED_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(Onboard_LED_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : DIS_A_Pin DIS_B_Pin DIS_C_Pin DIS_D_Pin
+                           DIS_E_Pin DIS_F_Pin DIS_G_Pin DIS_DP_Pin */
+  GPIO_InitStruct.Pin = DIS_A_Pin|DIS_B_Pin|DIS_C_Pin|DIS_D_Pin
+                          |DIS_E_Pin|DIS_F_Pin|DIS_G_Pin|DIS_DP_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+}
+
+/* USER CODE BEGIN 2 */
+
+/* USER CODE END 2 */
